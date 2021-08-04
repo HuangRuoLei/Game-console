f15a416f153280558713f2b4235d8b1660ca8a69
HuangRuoLei <69152711+HuangRuoLei@users.noreply.github.com>
2021-08-04 09:59:08 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -610,6 +610,52 @@ namespace HuLuMaoGame1 {
 	    OLED_WR_Byte((x&0x0f),OLED_CMD); 
     }
 
+    function drawShape(pixels: Array<Array<number>>) {
+        let x1 = 128
+        let y1 = 64
+        let x2 = 0
+        let y2 = 0
+        for (let i = 0; i < pixels.length; i++) {
+            if (pixels[i][0] < x1) {
+                x1 = pixels[i][0]
+            }
+            if (pixels[i][0] > x2) {
+                x2 = pixels[i][0]
+            }
+            if (pixels[i][1] < y1) {
+                y1 = pixels[i][1]
+            }
+            if (pixels[i][1] > y2) {
+                y2 = pixels[i][1]
+            }
+        }
+        let page1 = Math.floor(y1 / 8)
+        let page2 = Math.floor(y2 / 8)
+        let line = pins.createBuffer(2)
+        line[0] = 0x40
+        for (let x = x1; x <= x2; x++) {
+            for (let page = page1; page <= page2; page++) {
+                line[1] = 0x00
+                for (let i = 0; i < pixels.length; i++) {
+                    if (pixels[i][0] === x) {
+                        if (Math.floor(pixels[i][1] / 8) === page) {
+                            line[1] |= Math.pow(2, (pixels[i][1] % 8))
+                        }
+                    }
+                }
+                if (line[1] !== 0x00) {
+                    OLED_WR_Byte(0x21,OLED_CMD)
+                    OLED_WR_Byte(x,OLED_CMD)
+                    OLED_WR_Byte(x+1,OLED_CMD)
+                    OLED_WR_Byte(0x22,OLED_CMD)
+                    OLED_WR_Byte(page,OLED_CMD)
+                    OLED_WR_Byte(page+1,OLED_CMD)
+                    //line[1] |= pins.i2cReadBuffer(chipAdress, 2)[1]
+                    pins.i2cWriteBuffer(chipAdress, line, false)
+                }
+            }
+        }
+    }
 
      /**
      * 
@@ -782,4 +828,64 @@ namespace HuLuMaoGame1 {
         OLED_ShowChar(x,y,dat.toString())
     }
 
+        /**
+     * 
+     * @param index
+    */
+    //% blockId=HuLuMaoGame1_drawLine block="画线条，起点| x0 =%x0 y0 =%y0 终点| x1 =%x1 y1 =%y1"
+    //% weight=144
+    //% blockGap=10
+    //% color="#cc33ff"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function drawLine(x0: number, y0: number, x1: number, y1: number) {
+        y0*=8;y1*=8
+        let pixels: Array<Array<number>> = []
+        let kx: number, ky: number, c: number, i: number, xx: number, yy: number, dx: number, dy: number;
+        let targetX = x1
+        let targetY = y1
+        x1 -= x0; kx = 0; if (x1 > 0) kx = +1; if (x1 < 0) { kx = -1; x1 = -x1; } x1++;
+        y1 -= y0; ky = 0; if (y1 > 0) ky = +1; if (y1 < 0) { ky = -1; y1 = -y1; } y1++;
+        if (x1 >= y1) {
+            c = x1
+            for (i = 0; i < x1; i++ , x0 += kx) {
+                pixels.push([x0, y0])
+                c -= y1; if (c <= 0) { if (i != x1 - 1) pixels.push([x0 + kx, y0]); c += x1; y0 += ky; if (i != x1 - 1) pixels.push([x0, y0]); }
+                if (pixels.length > 20) {
+                    drawShape(pixels)
+                    pixels = []
+                    drawLine(x0, y0, targetX, targetY)
+                    return
+                }
+            }
+        } else {
+            c = y1
+            for (i = 0; i < y1; i++ , y0 += ky) {
+                pixels.push([x0, y0])
+                c -= x1; if (c <= 0) { if (i != y1 - 1) pixels.push([x0, y0 + ky]); c += y1; x0 += kx; if (i != y1 - 1) pixels.push([x0, y0]); }
+                if (pixels.length > 20) {
+                    drawShape(pixels)
+                    pixels = []
+                    drawLine(x0, y0, targetX, targetY)
+                    return
+                }
+            }
+        }
+        drawShape(pixels)
+    }
+
+       /**
+     * 
+     * @param index
+    */
+    //% blockId=HuLuMaoGame1_drawRectangle block="画矩形，起点| x0 =%x0 y0 =%y0 终点| x1 =%x1 y1 =%y1"
+    //% weight=143
+    //% blockGap=10
+    //% color="#cc33ff"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function drawRectangle(x0: number, y0: number, x1: number, y1: number) {
+        drawLine(x0, y0, x1, y0)
+        drawLine(x0, y1, x1, y1)
+        drawLine(x0, y0, x0, y1)
+        drawLine(x1, y0, x1, y1)
+    }
 }
